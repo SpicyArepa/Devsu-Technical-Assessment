07d8eaf1ef9bd6175b1d1c46f5c7350fdf893cad
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-06-09 00:32:14 -0400

    Red - pokemon-table test 1

--- src/components/pokemon-table/__test__/pokemon-table.test.js
+++ src/components/pokemon-table/__test__/pokemon-table.test.js
@@ -3,32 +3,33 @@ import { render, screen } from "@testing-library/react"
 import PokemonTable from '../PokemonTable'
 
 describe("Pokemon table", () => {
-  it("At the beginning must display a table with 1 row and 5 columns", () => {
+  it("At the beginning must display a table with 1 row and 5 columns",async () => {
+    render(<PokemonTable />);
+    expect(await screen.findAllByRole('table-header-row')).toHaveLength(1)
+    expect(await screen.findAllByRole('table-header-column')).toHaveLength(5)
+  })
+
+  xit("must display the headers of the table", () => {
     render(<PokemonTable />)
 
   })
 
-  it("must display the headers of the table", () => {
+  xit("must get the info from the API", () => {
     render(<PokemonTable />)
 
   })
 
-  it("must get the info from the API", () => {
+  xit("must display the info in their corresponding columns", () => {
     render(<PokemonTable />)
 
   })
 
-  it("must display the info in their corresponding columns", () => {
+  xit("It should display a new pokemon in another row", () => {
     render(<PokemonTable />)
 
   })
 
-  it("It should display a new pokemon in another row", () => {
-    render(<PokemonTable />)
-
-  })
-
-  describe("Each pokemon must have two buttons to edit and remove with a unique id", () => {
+  xdescribe("Each pokemon must have two buttons to edit and remove with a unique id", () => {
     it("Each pokemon must have two buttons", () => {
       render(<PokemonTable />)
 
