ea62640e353bc729b25215e139c0532fb95b07a6
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-06-09 01:51:29 -0400

    Green (Hardcode) - pokemon-table test 3

--- src/components/pokemon-table/PokemonTable.jsx
+++ src/components/pokemon-table/PokemonTable.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 const PokemonTable = () => {
+  const Poke = {
+    id: 7611,
+    name: 'Psyduck',
+    image: 'https://i.pinimg.com/736x/85/1e/cf/851ecf1b1c06cb071c37fb3c6de2ea4a.jpg',
+    type: 'fire',
+    hp: 100,
+    attack: 50,
+    defense: 30
+  }
   return (
     <table>
       <thead>
@@ -11,6 +20,18 @@ const PokemonTable = () => {
           <th role={'table-header-column'}>Acciones</th>
         </tr>
       </thead>
+      <tbody>
+        <tr role={'Pokemon'}>
+          <td role={'stat'} >{Poke.name}</td>
+          <td role={'stat'}>{Poke.image}</td>
+          <td role={'stat'}>{Poke.attack}</td>
+          <td role={'stat'}>{Poke.defense}</td>
+          <td role={'actions'}>
+            <button></button>
+            <button></button>
+          </td>
+        </tr>
+      </tbody>
     </table>
   )
 }
