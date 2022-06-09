7abda68ba48b4fd045100f375da1830d06dddb92
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-06-09 17:49:04 -0400

    Green - Button can be disable.

--- src/components/primary-button/PrimaryButton.jsx
+++ src/components/primary-button/PrimaryButton.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const PrimaryButton = ({icon , text, cb}) => {
+const PrimaryButton = ({icon , text, cb, disable = false}) => {
   return (
-    <button onClick={cb}>
+    <button onClick={cb} disabled={disable}>
       <div><img src={icon} alt="icon" role={'button-icon'}/></div>
       <div><span role={'button-text'}>{text}</span></div>
     </button>
