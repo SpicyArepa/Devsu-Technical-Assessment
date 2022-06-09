b5a435bf8b2c25f4dc7d6d9084038103c7aa33ec
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-06-09 00:36:48 -0400

    Green - pokemon-table test 1

---
+++ src/components/pokemon-table/PokemonTable.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+const PokemonTable = () => {
+  return (
+    <table>
+      <thead>
+        <tr role={'table-header-row'}>
+          <th role={'table-header-column'}></th>
+          <th role={'table-header-column'}></th>
+          <th role={'table-header-column'}></th>
+          <th role={'table-header-column'}></th>
+          <th role={'table-header-column'}></th>
+        </tr>
+      </thead>
+    </table>
+  )
+}
+
+export default PokemonTable
