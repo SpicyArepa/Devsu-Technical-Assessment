4021bd570deb836e598440127ad8fa461b6985f4
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-06-10 23:03:48 -0400

    Green - Edit case for PokeForm,  validate.js add function, fix PokeForm

--- src/components/poke-form/PokeForm.jsx
+++ src/components/poke-form/PokeForm.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import PrimaryButton from "../primary-button/PrimaryButton";
-import validate from "./validate";
+import {validate, compareData }from "./validate";
 
-const PokeForm = ( { cb } ) => {
+const PokeForm = ( { cb, closeFunction, pokemonData } ) => {
 
   const [error,setError] = useState({ error : 'empty form'})
   const initialInput = {
-    name : '',
-    image : '',
-    attack: 0,
-    defense: 0
+    name : pokemonData ? pokemonData.name : '',
+    image : pokemonData ? pokemonData.image : '',
+    attack: pokemonData ? pokemonData.attack : '',
+    defense: pokemonData ? pokemonData.defense : ''
   }
+
   const [input,setInput] = useState(initialInput)
   
   const handleChange = function (e) {
@@ -27,36 +28,36 @@ const PokeForm = ( { cb } ) => {
     setError(validate(input))
     
   },[input])
-
+  let saveDisable = Object.values(error).length > 1 || (pokemonData ? compareData(input,pokemonData) : false)
   return (
-    <form onSubmit={(e) => cb(e,input)} onChange={handleChange}>
-    <h3>Nuevo Pokemon</h3>
+    <form onSubmit={(e) => cb(e,input)}>
+    <h3> {pokemonData ? 'Editar Pokemon' : 'Nuevo Pokemon'}</h3>
     <div>
       <label htmlFor="name">Nombre:</label>
-      <input type="text" id="name" role={'name'}/>
+      <input type="text" id="name" role={'name'} value={input.name} onChange={handleChange}/>
     </div>
 
     <div>
       <label htmlFor="image">Imagen:</label>
-      <input type="text" id="image" role={'image'}/>
+      <input type="text" id="image" role={'image'} value={input.image} onChange={handleChange}/>
     </div>
     <div>
 
       <label htmlFor="attack">Ataque:</label>
-      <input type="range" id="attack" role={'attack'} min="0" max="100"/>
+      <input type="range" id="attack" role={'attack'} min="0" max="100" onChange={handleChange} value={input.attack}/>
     </div>
 
     <div>
       <label htmlFor="defense">Defensa:</label>
-      <input type="range" id="defense" role={'defense'} min="0" max="100"/>
+      <input type="range" id="defense" role={'defense'} min="0" max="100" onChange={handleChange} value={input.defense}/>
     </div>
 
     <div role={'save-button'}>
-      <PrimaryButton text={'Guardar'} disable={Object.values(error).length > 1} type='submit'/>
+      <PrimaryButton text={'Guardar'} disable={saveDisable} type='submit'/>
     </div>
 
     <div role={'cancel-button'}>
-      <PrimaryButton text={'Cancelar'}/>
+      <PrimaryButton text={'Cancelar'} cb={closeFunction}/>
     </div>
     </form>
   );
--- src/components/poke-form/validate.js
+++ src/components/poke-form/validate.js
@@ -1,28 +1,35 @@
-const validate = (input) => {
-    let error = {};
-    if (!input.name) {
-      error.name = "";
-    } else if (!/^.{8,15}$/.test(input.name)) {
-      error.name =
-        "Too long or too short";
-    }
+export const validate = (input) => {
+  let error = {};
+  if (!input.name) {
+    error.name = "";
+  } else if (!/^.{8,15}$/.test(input.name)) {
+    error.name = "Too long or too short";
+  }
 
-    if (!input.image) {
-      error.image = "image required";
-    } else if (!/(https?:\/\/.*\.(?:png|jpg))/i.test(input.image)) {
-      error.image =
-        "Must be a url image in png or jpg format";
-    }
+  if (!input.image) {
+    error.image = "image required";
+  } else if (!/(https?:\/\/.*\.(?:png|jpg))/i.test(input.image)) {
+    error.image = "Must be a url image in png or jpg format";
+  }
 
-    if (input.attack <= 0 || input.attack > 100 ) {
-      error.attack = "attack must be between 1 and 100";
-    } 
+  if (input.attack <= 0 || input.attack > 100) {
+    error.attack = "attack must be between 1 and 100";
+  }
 
-    if (input.defense <= 0 || input.defense > 100 ) {
-      error.defense = "defense must be between 1 and 100";
-    }
+  if (input.defense <= 0 || input.defense > 100) {
+    error.defense = "defense must be between 1 and 100";
+  }
 
-    return error;
-  };
+  return error;
+};
 
-  export default validate
+export const compareData = (input, pokemonData) => {
+  if (
+    input.name === pokemonData.name &&
+    input.image === pokemonData.image &&
+    input.attack === pokemonData.attack &&
+    input.defense === pokemonData.defense
+  )
+    return true;
+  return false;
+};
