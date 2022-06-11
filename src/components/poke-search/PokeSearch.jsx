379aab439a2b3e154f70f2f5116f9e1b02ae3666
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-06-11 17:22:46 -0400

    Features and test done

--- src/components/poke-search/PokeSearch.jsx
+++ src/components/poke-search/PokeSearch.jsx
@@ -1,16 +1,28 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getPokemons } from "../../redux/asyncActions/pokemon/getPokemons";
+import { searchPokemons } from "../../redux/asyncActions/pokemon/searchPokemons";
+import lupa from '../../assets/lupa.png'
 
 const PokeSearch = ( ) => {
-
+  const dispatch = useDispatch()
+  const [search,setSearch] = useState('')
+  const handlePress = (e) => {
+    if (e.key === 'Enter') {
+      search ? dispatch(searchPokemons(search)) : dispatch(getPokemons())
+    }
+  }
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
 
   return (
     <div>
       <div>
-        <img src="" alt="icon" role={'icon'} />
+        <img src={lupa} alt="icon" role={'icon'} width={25}/>
       </div>
       <div>
-        <input type="text" placeholder="Buscar" role={'input-search'}/>
+        <input type="search" onKeyPress={handlePress} placeholder="Buscar" role={'input-search'} onChange={handleChange} value={search}/>
       </div>
     </div>
   );
