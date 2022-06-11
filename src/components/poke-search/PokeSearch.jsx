0825c0525930cc4a12f6d51ce976be76769854c5
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-06-11 16:16:17 -0400

    Green - test 1 and 2

---
+++ src/components/poke-search/PokeSearch.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const PokeSearch = ( ) => {
+
+
+  return (
+    <div>
+      <div>
+        <img src="" alt="icon" role={'icon'} />
+      </div>
+      <div>
+        <input type="text" placeholder="Buscar" role={'input-search'}/>
+      </div>
+    </div>
+  );
+};
+export default PokeSearch;
