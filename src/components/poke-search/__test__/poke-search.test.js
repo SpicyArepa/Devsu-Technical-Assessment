267937939194c4c4782962c82aa2908ece36c4f4
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-06-11 16:09:37 -0400

    Red - test 1 and 2 PokeSearch

---
+++ src/components/poke-search/__test__/poke-search.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { getPokemonById, getPokemons } from "../../../redux/features/pokemon/pokemonSlice";
+import { fireEvent, render, within } from '../../../utils/__test-utils__/test-utils';
+import store from "../../../redux/store";
+import PokeSearch from "../PokeSearch";
+import { act } from "react-test-renderer";
+import { Provider } from "react-redux";
+
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+
+describe("Pokemon Form", () => {
+
+  it("must have a icon", () => {
+    const {getByAltText} = render(<PokeSearch />);
+    expect(getByAltText('icon'))
+  })
+
+  it("must display a input with a placeholder", async () => {
+    const {getByRole} = render(<PokeSearch />)
+    const input = getByRole('input-search')
+    expect(await within(input).findByPlaceholderText(/Buscar/i))
+  })
+
+
+});
